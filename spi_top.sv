2682381e9190d3174ec72b4a72664b102717fd8c
arkeks <theincrediblelynx@yandex.ru>
2023-04-02 16:41:06 +0300

    Added top level logic; set up for omdazz board

--- spi_top.sv
+++ spi_top.sv
@@ -1,31 +1,57 @@
 module spi_top
 #(
-  parameter SIZE = 8
+  parameter F_SIZE = 8,
+            F_NUM  = 4,
+				FC_SIZE = $clog2(F_NUM) + 1,
+				FREQ   = 1000
 )
 (
-  input         clk,
-  input         reset_n,   // negative reset
-  input [3:0]   key_sw,    // negative tactile switches
+  input  logic        clk,
+  input  logic        rst_n,   // negative reset
+  input  logic [3:0]  key_sw,    // negative tactile switches       
+  input  logic        miso_i,
   
-  output [13:0] gpio,
-  output [7:0]  abcdefgh, // displayed letter
-  output [3:0]  digit,    // 4 indicators
-  output [3:0]  led
+  output logic        mosi_o,
+  output logic        cs_o,
+  output logic        sclk_o,
+  output logic [7:0]  abcdefgh, // displayed letter
+  output logic [3:0]  digit,    // 4 indicators
+  output logic [3:0]  led
 );
 
-logic        start_i;
-logic [25:0] cnt;
-logic [7:0]  tx_data_i;
-logic [7:0]  rx_data_o;
-logic        MISO, CS, SCLK, MOSI;
+logic                 start_i, rst;
+logic [FC_SIZE - 1:0] f_cnt;
+logic [7:0]           tx_buff [0:3];
+logic [7:0]           tx_data_i;
+logic [7:0]           rx_data_o;
+logic                 MISO, CS, SCLK, MOSI;
 
 
-// logic for pressings counting
- 
+assign rst = ~rst_n;
+
+// start transmit by pressing
+assign start_i = ~key_sw[0];
+
+// led indication
+assign led[0] = ~key_sw[0];
+
+// hard coding message "FPGA"
+assign tx_buff[0] = 'h66; // F
+assign tx_buff[1] = 'h70; // P
+assign tx_buff[2] = 'h67; // G
+assign tx_buff[3] = 'h61; // A
+
+
+// changing tx frame every CS rising edge
+always @ (posedge clk or posedge rst)
+  if (rst)
+    tx_data_i = tx_buff[0];
+  else if (CS)
+    tx_data_i = tx_buff[f_cnt];
 
 
 
-master
+master #( .FREQ(FREQ), .F_NUM(F_NUM), .F_SIZE(F_SIZE) )
   test
 	(
 		clk,
@@ -37,10 +63,17 @@ master
 		MISO,
 		CS,
 		SCLK,
-		MOSI
+		MOSI,
+		f_cnt
 	);
 	
-	
+assign MISO = miso_i;
+
+assign mosi_o = MOSI;
+
+assign cs_o   = CS;
+
+assign sclk_o = SCLK;
 	
 /* // counter for start_i signal rising
 always_ff @ (posedge clk or posedge rst)
